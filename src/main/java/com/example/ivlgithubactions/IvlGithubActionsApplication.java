7e5ed81890c20b2eed8313aa6688edfcb8277f7a
ivanevla <mail@ivanevla.net>
2023-04-22 00:20:05 +0300

    git init

---
+++ src/main/java/com/example/ivlgithubactions/IvlGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ivlgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvlGithubActionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvlGithubActionsApplication.class, args);
+    }
+
+}
