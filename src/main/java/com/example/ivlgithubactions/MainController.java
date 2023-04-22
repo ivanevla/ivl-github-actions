938be0b23ed3aa3096191112149f19263d7d0949
ivanevla <mail@ivanevla.net>
2023-04-23 01:01:38 +0300

    update deploy.yml

--- src/main/java/com/example/ivlgithubactions/MainController.java
+++ src/main/java/com/example/ivlgithubactions/MainController.java
@@ -1,6 +1,5 @@
 package com.example.ivlgithubactions;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String hello(){
-        return "Hello";
+        return "Hello all!!";
     }
 }
