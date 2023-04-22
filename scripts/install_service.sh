4c02d2cd45461fe726a117aca873be4bba9e28b6
ivanevla <mail@ivanevla.net>
2023-04-23 00:54:37 +0300

    update deploy.yml

--- scripts/install_service.sh
+++ scripts/install_service.sh
@@ -2,3 +2,14 @@
 
 mkdir "/appcfg/app"
 cp
+
+
+sudo ln -s /software/app/finances-0.0.1-SNAPSHOT.jar /etc/init.d/finances-api
+
+usupp@spring-deploy-test:/appcfg$ sudo ln -s /appcfg/jar/ivl-github-actions.jar /etc/init.d/ivl-github-actions
+/etc/init.d/ivl-github-actions
+sudo systemctl daemon-reload
+
+sudo systemctl enable ivl-github-actions
+sudo systemctl start ivl-github-actions
+sudo systemctl status ivl-github-actionsd
